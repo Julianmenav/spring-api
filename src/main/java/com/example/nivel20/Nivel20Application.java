d5efa59d5cf09f9127e56230be46920dc84c0e5f
Julianmenav <julianmenav2@gmail.com>
2023-08-23 11:38:12 +0200

    feat: Add spring security & mysql driver dependencies

--- src/main/java/com/example/nivel20/Nivel20Application.java
+++ src/main/java/com/example/nivel20/Nivel20Application.java
@@ -1,9 +1,5 @@
 package com.example.nivel20;
 
-import com.example.nivel20.entities.Character;
-import com.example.nivel20.entities.Mage;
-import com.example.nivel20.entities.Marksman;
-import com.example.nivel20.entities.Warrior;
 import com.example.nivel20.repositories.CharacterRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,18 +13,6 @@ public class Nivel20Application {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Nivel20Application.class, args);
 		CharacterRepository characterRepo = context.getBean(CharacterRepository.class);
-
-		Character personaje1 = new Warrior("Zink");
-		Character personaje2 = new Mage("judas");
-		Character personaje3 = new Marksman("kixy");
-
-//		CRUD
-//		Create
-		characterRepo.save(personaje1);
-		characterRepo.save(personaje3);
-		characterRepo.save(personaje2);
-
-
 	}
 
 }
